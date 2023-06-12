9f3c1e678bc6a0ad7e5f51c2f21f5abc4b9f3375
Kareem Mahlees <kareemmahlees@gmail.com>
2023-06-12 22:05:04 +0300

    finalize project card mobile version

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,11 +1,15 @@
 "use client";
 import Image from "next/image";
-import { FC } from "react";
+import { FC, ReactElement } from "react";
 import { useInView } from "react-intersection-observer";
 import { RoughNotation } from "react-rough-notation";
 import { Tooltip } from "@chakra-ui/react";
-import { ArrowRight } from "lucide-react";
+import { Inter } from "next/font/google";
 import Link from "next/link";
+import { SocialIcon } from "./ui/Icon";
+import { JsxElement } from "typescript";
+
+const inter = Inter({ subsets: ["latin"], weight: "400" });
 
 interface ProjectsProps {}
 
@@ -37,34 +41,20 @@ const Projects: FC<ProjectsProps> = ({}) => {
             {
               src: "https://python-poetry.org/images/logo-origami.svg",
               toolTip: "Poetry",
-              width: 25,
-              height: 25,
+              width: 22,
+              height: 22,
             },
           ]}
-        >
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus
-          praesentium quaerat voluptatibus excepturi fugit sit atque, hic illum
-          inventore tempore voluptate cupiditate doloribus libero ex assumenda
-          asperiores facere? Veritatis, consequatur?
-        </ProjectCard>
-
-        <ProjectCard
-          imgSrc="/project_test_img.svg"
-          projectName="Cachzilla"
-          techIcons={[
+          socialIcons={[
             {
-              src: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/python-colored.svg",
-              toolTip: "Python",
+              link: "https://github.com/kareemmahlees/cachezilla",
+              alt: "github project link",
+              src: "https://img.icons8.com/glyph-neue/128/EBEBEB/github.png",
             },
             {
-              src: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/git-colored.svg",
-              toolTip: "Git",
-            },
-            {
-              src: "https://python-poetry.org/images/logo-origami.svg",
-              toolTip: "Poetry",
-              width: 25,
-              height: 25,
+              link: "https://medium.com/@kareemmahlees/understand-and-build-a-caching-system-from-scratch-f931fb07f62a",
+              alt: "medium project link",
+              src: "https://img.icons8.com/ios-filled/100/EBEBEB/medium-logo.png",
             },
           ]}
         >
@@ -90,28 +80,27 @@ interface ProjectCardProps {
     height?: number;
     toolTip: string;
   }[];
+  socialIcons: { link: string; alt: string; src: string }[];
 }
 
 const ProjectCard: FC<ProjectCardProps> = ({
   children,
-  imgSrc,
   techIcons,
+  socialIcons,
   projectName,
 }) => {
   return (
     <article
       id="projects"
-      className="scroll-my-40 lg:scroll-my-60 mx-7 relative flex flex-col items-center bg-[#1f1435] rounded-lg"
+      className="scroll-my-40 lg:scroll-my-60 mx-7 relative flex flex-col items-center bg-[#170f28] rounded-lg"
     >
-      <Image
-        alt="project img"
-        src={imgSrc}
-        width={400}
-        height={400}
-        className="rounded-lg"
-      />
-      <div className="rounded-xl relative -top-5 backdrop-blur-xl pt-10 space-y-5 px-4">
-        <p className="text-center ">{children}</p>
+      <div className="relative h-44 w-full bg-white flex items-center justify-center overflow-hidden rounded-xl bg-[url(/polygon.svg)] bg-contain">
+        <h3 className="text-gray-500 font-bold capitalize text-4xl">
+          {projectName}
+        </h3>
+      </div>
+      <div className="rounded-xl relative -top-4 backdrop-blur-xl pt-10 space-y-5 px-4">
+        <p className="text-center text-[#eef1ff]">{children}</p>
         <div className="flex items-center gap-4 justify-center">
           {techIcons.map((icon, index) => (
             <Tooltip key={index} label={icon.toolTip}>
@@ -119,20 +108,21 @@ const ProjectCard: FC<ProjectCardProps> = ({
                 key={index}
                 alt="tech icon"
                 src={icon.src}
-                width={icon.width || 30}
-                height={icon.height || 30}
+                width={icon.width || 27}
+                height={icon.height || 27}
               />
             </Tooltip>
           ))}
         </div>
-        <div className="flex items-center w-full justify-center group hover:cursor-pointer">
-          <Link
-            href={`/projects/${projectName}`}
-            className="font-semibold underline underline-offset-4"
-          >
-            read more
-          </Link>
-          <ArrowRight className="group-hover:translate-x-3 transition-all group-hover:opacity-100 opacity-0" />
+        <div className="flex justify-center items-center gap-4">
+          {socialIcons.map((icon, index) => (
+            <SocialIcon
+              alt={icon.alt}
+              key={index}
+              src={icon.src}
+              link={icon.link}
+            />
+          ))}
         </div>
       </div>
     </article>
