9f2a44ef9f386a77f751d4183cbf974935eac967
Kareem Mahlees <kareemmahlees@gmail.com>
2024-01-18 18:34:41 +0200

    refactor: heading section

---
+++ website/src/consts.ts
@@ -0,0 +1,4 @@
+export const CV_PDF_LINK =
+  "https://docs.google.com/document/u/0/export?format=pdf&id=1js7jkFavUT6LgHqmUikKH0Fucz5Akjcbi0yNuR3XAno";
+export const CV_DOCX_LINK =
+  "https://docs.google.com/document/u/0/export?format=docx&id=1js7jkFavUT6LgHqmUikKH0Fucz5Akjcbi0yNuR3XAno";
